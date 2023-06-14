7481c9055f961d97749109442a4089a10ea2cc1e
Kelvin Fichter <kelvin@optimism.io>
2023-06-14 18:51:39 -0400

    fix: broken l2geth init script

--- scripts/init-l2geth.sh
+++ scripts/init-l2geth.sh
@@ -22,4 +22,4 @@ if [ ! -d "$GETH_CHAINDATA_DIR" ]; then
   echo "$GETH_CHAINDATA_DIR missing, running init"
   geth init --datadir="$GETH_DATA_DIR" "$L2GETH_GENESIS_URL" "$L2GETH_GENESIS_HASH"
   echo "geth init complete"
-else
+fi
