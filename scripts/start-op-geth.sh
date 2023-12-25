2463fdf19f294d446a08cf4cea4c94121ec331df
Chomtana <Chomtana001@gmail.com>
2023-12-25 18:23:51 +0700

    feat: custom chain support 1

--- scripts/start-op-geth.sh
+++ scripts/start-op-geth.sh
@@ -8,34 +8,39 @@ while [ ! -f /shared/initialized.txt ]; do
 done
 
 # Start op-geth.
-exec geth \
-  --datadir="$BEDROCK_DATADIR" \
-  --http \
-  --http.corsdomain="*" \
-  --http.vhosts="*" \
-  --http.addr=0.0.0.0 \
-  --http.port=8545 \
-  --http.api=web3,debug,eth,txpool,net,engine \
-  --ws \
-	--ws.addr=0.0.0.0 \
-	--ws.port=8546 \
-	--ws.origins="*" \
-	--ws.api=debug,eth,txpool,net,engine,web3 \
-  --metrics \
-  --metrics.influxdb \
-  --metrics.influxdb.endpoint=http://influxdb:8086 \
-  --metrics.influxdb.database=opgeth \
-  --syncmode=full \
-  --gcmode="$NODE_TYPE" \
-  --nodiscover \
-  --maxpeers=0 \
-  --networkid=420 \
-  --authrpc.vhosts="*" \
-  --authrpc.addr=0.0.0.0 \
-  --authrpc.port=8551 \
-  --authrpc.jwtsecret=/shared/jwt.txt \
-  --rollup.sequencerhttp="$BEDROCK_SEQUENCER_HTTP" \
-  --rollup.disabletxpoolgossip=true \
-  --rollup.historicalrpc="${OP_GETH__HISTORICAL_RPC:-http://l2geth:8545}" \
-  --op-network=$NETWORK_NAME \
-  $@
+if [ "$IS_CUSTOM_CHAIN" = "true" ]; then
+
+else
+  exec geth \
+    --datadir="$BEDROCK_DATADIR" \
+    --http \
+    --http.corsdomain="*" \
+    --http.vhosts="*" \
+    --http.addr=0.0.0.0 \
+    --http.port=8545 \
+    --http.api=web3,debug,eth,txpool,net,engine \
+    --ws \
+    --ws.addr=0.0.0.0 \
+    --ws.port=8546 \
+    --ws.origins="*" \
+    --ws.api=debug,eth,txpool,net,engine,web3 \
+    --metrics \
+    --metrics.influxdb \
+    --metrics.influxdb.endpoint=http://influxdb:8086 \
+    --metrics.influxdb.database=opgeth \
+    --syncmode=full \
+    --gcmode="$NODE_TYPE" \
+    --nodiscover \
+    --maxpeers=0 \
+    --networkid=420 \
+    --authrpc.vhosts="*" \
+    --authrpc.addr=0.0.0.0 \
+    --authrpc.port=8551 \
+    --authrpc.jwtsecret=/shared/jwt.txt \
+    --rollup.sequencerhttp="$BEDROCK_SEQUENCER_HTTP" \
+    --rollup.disabletxpoolgossip=true \
+    --rollup.historicalrpc="${OP_GETH__HISTORICAL_RPC:-http://l2geth:8545}" \
+    --op-network=$NETWORK_NAME \
+    $@
+fi
+
--- scripts/start-op-node.sh
+++ scripts/start-op-node.sh
@@ -7,18 +7,37 @@ while [ ! -f /shared/initialized.txt ]; do
   sleep 60
 done
 
-# Start op-node.
-exec op-node \
-  --l1=$OP_NODE__RPC_ENDPOINT \
-  --l2=http://op-geth:8551 \
-  --network=$NETWORK_NAME \
-  --rpc.addr=127.0.0.1 \
-  --rpc.port=9545 \
-  --l2.jwt-secret=/shared/jwt.txt \
-  --l1.trustrpc \
-  --l1.rpckind=$OP_NODE__RPC_TYPE \
-  --metrics.enabled \
-  --metrics.addr=0.0.0.0 \
-  --metrics.port=7300 \
-  --rollup.load-protocol-versions=true \
-  $@
+# Check if OP_NODE__IS_CUSTOM_CHAIN is true
+if [ "$IS_CUSTOM_CHAIN" = "true" ]; then
+  # Start op-node.
+  exec op-node \
+    --l1=$OP_NODE__RPC_ENDPOINT \
+    --l2=http://op-geth:8551 \
+    --network=$NETWORK_NAME \
+    --rpc.addr=127.0.0.1 \
+    --rpc.port=9545 \
+    --l2.jwt-secret=/shared/jwt.txt \
+    --l1.trustrpc \
+    --l1.rpckind=$OP_NODE__RPC_TYPE \
+    --metrics.enabled \
+    --metrics.addr=0.0.0.0 \
+    --metrics.port=7300 \
+    --rollup.config=/chainconfig/rollup.json \
+    $@
+else
+  # Start op-node.
+  exec op-node \
+    --l1=$OP_NODE__RPC_ENDPOINT \
+    --l2=http://op-geth:8551 \
+    --network=$NETWORK_NAME \
+    --rpc.addr=127.0.0.1 \
+    --rpc.port=9545 \
+    --l2.jwt-secret=/shared/jwt.txt \
+    --l1.trustrpc \
+    --l1.rpckind=$OP_NODE__RPC_TYPE \
+    --metrics.enabled \
+    --metrics.addr=0.0.0.0 \
+    --metrics.port=7300 \
+    --rollup.load-protocol-versions=true \
+    $@
+fi
