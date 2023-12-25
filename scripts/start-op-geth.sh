92e0a95781a7e9fac9095b3bc43e6394fab17f6b
Chomtana <Chomtana001@gmail.com>
2023-12-25 18:53:36 +0700

    feat: use extended args for custom chain

--- scripts/start-op-geth.sh
+++ scripts/start-op-geth.sh
@@ -7,40 +7,47 @@ while [ ! -f /shared/initialized.txt ]; do
   sleep 60
 done
 
-# Start op-geth.
-if [ "$IS_CUSTOM_CHAIN" = "true" ]; then
-
-else
-  exec geth \
-    --datadir="$BEDROCK_DATADIR" \
-    --http \
-    --http.corsdomain="*" \
-    --http.vhosts="*" \
-    --http.addr=0.0.0.0 \
-    --http.port=8545 \
-    --http.api=web3,debug,eth,txpool,net,engine \
-    --ws \
-    --ws.addr=0.0.0.0 \
-    --ws.port=8546 \
-    --ws.origins="*" \
-    --ws.api=debug,eth,txpool,net,engine,web3 \
-    --metrics \
-    --metrics.influxdb \
-    --metrics.influxdb.endpoint=http://influxdb:8086 \
-    --metrics.influxdb.database=opgeth \
-    --syncmode=full \
-    --gcmode="$NODE_TYPE" \
-    --nodiscover \
-    --maxpeers=0 \
-    --networkid=420 \
-    --authrpc.vhosts="*" \
-    --authrpc.addr=0.0.0.0 \
-    --authrpc.port=8551 \
-    --authrpc.jwtsecret=/shared/jwt.txt \
-    --rollup.sequencerhttp="$BEDROCK_SEQUENCER_HTTP" \
-    --rollup.disabletxpoolgossip=true \
-    --rollup.historicalrpc="${OP_GETH__HISTORICAL_RPC:-http://l2geth:8545}" \
-    --op-network=$NETWORK_NAME \
-    $@
+if [ -z "$IS_CUSTOM_CHAIN" ]; then
+  if [ "$NETWORK_NAME" == "op-mainnet" ] || [ "$NETWORK_NAME" == "op-goerli" ]; then
+    EXTENDED_ARG="$EXTENDED_ARG --rollup.historicalrpc=${OP_GETH__HISTORICAL_RPC:-http://l2geth:8545} --op-network=$NETWORK_NAME"
+  else
+    EXTENDED_ARG="$EXTENDED_ARG --op-network=$NETWORK_NAME"
+  fi
 fi
+
+# Init genesis if custom chain
+if [ -n "$IS_CUSTOM_CHAIN" ]; then
+  geth init --datadir="$BEDROCK_DATADIR" /chainconfig/genesis.json
+fi
+
+# Start op-geth.
+exec geth \
+  --datadir="$BEDROCK_DATADIR" \
+  --http \
+  --http.corsdomain="*" \
+  --http.vhosts="*" \
+  --http.addr=0.0.0.0 \
+  --http.port=8545 \
+  --http.api=web3,debug,eth,txpool,net,engine \
+  --ws \
+  --ws.addr=0.0.0.0 \
+  --ws.port=8546 \
+  --ws.origins="*" \
+  --ws.api=debug,eth,txpool,net,engine,web3 \
+  --metrics \
+  --metrics.influxdb \
+  --metrics.influxdb.endpoint=http://influxdb:8086 \
+  --metrics.influxdb.database=opgeth \
+  --syncmode=full \
+  --gcmode="$NODE_TYPE" \
+  --nodiscover \
+  --maxpeers=0 \
+  --networkid=420 \
+  --authrpc.vhosts="*" \
+  --authrpc.addr=0.0.0.0 \
+  --authrpc.port=8551 \
+  --authrpc.jwtsecret=/shared/jwt.txt \
+  --rollup.sequencerhttp="$BEDROCK_SEQUENCER_HTTP" \
+  --rollup.disabletxpoolgossip=true \
+  $EXTENDED_ARG $@
 
--- scripts/start-op-node.sh
+++ scripts/start-op-node.sh
@@ -7,37 +7,22 @@ while [ ! -f /shared/initialized.txt ]; do
   sleep 60
 done
 
-# Check if OP_NODE__IS_CUSTOM_CHAIN is true
-if [ "$IS_CUSTOM_CHAIN" = "true" ]; then
-  # Start op-node.
-  exec op-node \
-    --l1=$OP_NODE__RPC_ENDPOINT \
-    --l2=http://op-geth:8551 \
-    --network=$NETWORK_NAME \
-    --rpc.addr=127.0.0.1 \
-    --rpc.port=9545 \
-    --l2.jwt-secret=/shared/jwt.txt \
-    --l1.trustrpc \
-    --l1.rpckind=$OP_NODE__RPC_TYPE \
-    --metrics.enabled \
-    --metrics.addr=0.0.0.0 \
-    --metrics.port=7300 \
-    --rollup.config=/chainconfig/rollup.json \
-    $@
+if [ -n "$IS_CUSTOM_CHAIN" ]; then
+  EXTENDED_ARG="$EXTENDED_ARG --rollup.config=/chainconfig/rollup.json"
 else
-  # Start op-node.
-  exec op-node \
-    --l1=$OP_NODE__RPC_ENDPOINT \
-    --l2=http://op-geth:8551 \
-    --network=$NETWORK_NAME \
-    --rpc.addr=127.0.0.1 \
-    --rpc.port=9545 \
-    --l2.jwt-secret=/shared/jwt.txt \
-    --l1.trustrpc \
-    --l1.rpckind=$OP_NODE__RPC_TYPE \
-    --metrics.enabled \
-    --metrics.addr=0.0.0.0 \
-    --metrics.port=7300 \
-    --rollup.load-protocol-versions=true \
-    $@
+  EXTENDED_ARG="$EXTENDED_ARG --network=$NETWORK_NAME --rollup.load-protocol-versions=true"
 fi
+
+# Start op-node.
+exec op-node \
+  --l1=$OP_NODE__RPC_ENDPOINT \
+  --l2=http://op-geth:8551 \
+  --rpc.addr=127.0.0.1 \
+  --rpc.port=9545 \
+  --l2.jwt-secret=/shared/jwt.txt \
+  --l1.trustrpc \
+  --l1.rpckind=$OP_NODE__RPC_TYPE \
+  --metrics.enabled \
+  --metrics.addr=0.0.0.0 \
+  --metrics.port=7300 \
+  $EXTENDED_ARG $@
