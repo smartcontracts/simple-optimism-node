5c74d573c7775ee6934e8583a246ad72494dd6e0
Chomtana <Chomtana001@gmail.com>
2024-06-05 00:55:29 +0700

    Expose op-node port for debugability

--- scripts/start-op-node.sh
+++ scripts/start-op-node.sh
@@ -17,7 +17,7 @@ fi
 exec op-node \
   --l1=$OP_NODE__RPC_ENDPOINT \
   --l2=http://op-geth:8551 \
-  --rpc.addr=127.0.0.1 \
+  --rpc.addr=0.0.0.0 \
   --rpc.port=9545 \
   --l2.jwt-secret=/shared/jwt.txt \
   --l1.trustrpc \
